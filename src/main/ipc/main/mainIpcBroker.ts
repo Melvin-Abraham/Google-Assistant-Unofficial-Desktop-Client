337cd563e4afff12c5dbebf82dd4092a16fda353
Melvin. L. Abraham <melvinredvirus@gmail.com>
2021-12-05 23:29:00 +0530

    feat(main): Support renderer sync/async IPC request
    - Implement sync/async IPC requests for renderer in MainIpcBroker's `on`
      method.
    
    - Modify `on` listener signature for "app:getAppConfig" in main
    
    - Move types for main ipc to types.ts
    
    - Add tsdocs for main ipc utils

--- src/main/ipc/main/mainIpcBroker.ts
+++ src/main/ipc/main/mainIpcBroker.ts
@@ -1,7 +1,8 @@
 import { ipcMain, IpcMainEvent } from 'electron';
 import { serialize } from 'main/ipc/common/ipcUtils';
 import { AssistantAppConfig } from 'main/utils/config/types';
-import { IpcMainOutboundMessage, InboundIpcChannel, IpcTarget } from './utils';
+import { isRendererSyncRequest, isRendererAsyncRequest } from './utils';
+import { IpcMainOutboundMessage, InboundIpcChannel, IpcTarget } from './types';
 
 export class MainIpcBroker {
   /**
@@ -37,14 +38,28 @@ export class MainIpcBroker {
   }
 
   static on(channel: 'app:quit', listener: (event: IpcMainEvent) => void): void;
-  static on(channel: 'app:getAppConfig', listener: (event: IpcMainEvent) => void): void;
+  static on(channel: 'app:getAppConfig', listener: (event: IpcMainEvent) => AssistantAppConfig): void;
   static on(channel: 'app:setAppConfig', listener: (event: IpcMainEvent, newConfig: AssistantAppConfig) => void): void;
   static on(channel: 'window:closeAssistantWindow', listener: (event: IpcMainEvent) => void): void;
   static on(channel: 'window:minimizeAssistantWindow', listener: (event: IpcMainEvent) => void): void;
 
   static on(channel: InboundIpcChannel, listener: Function) {
+    if (isRendererAsyncRequest(channel)) {
+      ipcMain.handle(channel, (event, ...args) => {
+        const returnValue = listener(event, ...args);
+        return returnValue;
+      });
+
+      return;
+    }
+
     ipcMain.on(channel, (event, ...args) => {
-      listener(event, ...args);
+      const returnValue = listener(event, ...args);
+
+      if (isRendererSyncRequest(channel)) {
+        // eslint-disable-next-line no-param-reassign
+        event.returnValue = returnValue;
+      }
     });
   }
 }
--- src/main/ipc/main/utils.ts
+++ src/main/ipc/main/utils.ts
@@ -1,17 +1,51 @@
-import { outboundIpcChannelsWhitelist as rendererOutboundChannels } from 'main/ipc/renderer/utils';
+import {
+  OutboundIpcChannel as RendererOutboundChannel,
+  outboundIpcChannelsWhitelist as rendererOutboundChannels,
+} from 'main/ipc/renderer/utils';
 
-export type OutboundIpcChannel = typeof outboundIpcChannels[number];
-export type InboundIpcChannel = typeof inboundIpcChannels[number];
-
-export interface IpcMainOutboundMessage {
-  ipcChannel: OutboundIpcChannel;
-  args: any[];
-}
-
-export type IpcTarget = 'renderer';
-
+/**
+ * Known IPC channels for emitting IPC events to external
+ * process and services
+ */
 export const outboundIpcChannels = [] as const;
 
+/**
+ * Known IPC channels for listening inbound IPC events
+ */
 export const inboundIpcChannels = [
   ...rendererOutboundChannels,
 ] as const;
+
+/**
+ * IPC requests which are made **synchronously** by the _renderer_
+ * process. These channels expect a response to be returned back.
+ */
+const rendererSyncRequests: RendererOutboundChannel[] = [
+  'app:getAppConfig',
+];
+
+/**
+ * IPC requests which are made **asynchronously** by the _renderer_
+ * process. These channels expect a response to be returned back.
+ */
+const rendererAsyncRequests: RendererOutboundChannel[] = [];
+
+/**
+ * Returns `true`, if the given channel is a synchronous request
+ * from renderer process.
+ *
+ * @param channel
+ */
+export function isRendererSyncRequest(channel: string) {
+  return (rendererSyncRequests as string[]).includes(channel);
+}
+
+/**
+ * Returns `true`, if the given channel is a synchronous request
+ * from renderer process.
+ *
+ * @param channel
+ */
+export function isRendererAsyncRequest(channel: string) {
+  return (rendererAsyncRequests as string[]).includes(channel);
+}
