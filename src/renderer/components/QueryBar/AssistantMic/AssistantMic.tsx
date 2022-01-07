622ee17e9d6b4531a84bcd058831a99b8532a8e5
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-01-07 11:38:30 +0530

    refactor: Minor refactoring in AssistantMic
    - Fix call to `off()` method in AssistantMic
    - Stop mic when end of utterance event is emitted by the assistant
    - Add support for immediate response
    - Stop audio player playback when starting the mic

--- src/renderer/components/QueryBar/AssistantMic/AssistantMic.tsx
+++ src/renderer/components/QueryBar/AssistantMic/AssistantMic.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import gassist from 'gassist';
 import { microphone } from 'lib/audio/microphone';
 import { audioPlayer } from 'lib/audio/audioPlayer';
 import AssistantMicMain from './AssistantMicMain';
@@ -13,17 +14,37 @@ function AssistantMic() {
   const [isMicDotsVisible, setMicDotsVisibility] = useState(false);
 
   useEffect(() => {
-    microphone.on('mic:started', () => {
-      setMicActive(true);
+    const onMicStarted = () => setMicActive(true);
+    const onMicStopped = () => setMicActive(false);
+
+    microphone.on('mic:started', onMicStarted);
+    microphone.on('mic:stopped', onMicStopped);
+
+    // Stop sending mic data to assistant once the user is done speaking
+    gassist.assistant.onEndOfUtterance(() => {
+      microphone.stop();
     });
 
-    microphone.on('mic:stopped', () => {
-      setMicActive(false);
+    // Handle mic stop request
+    gassist.assistant.onRequestStopMic(() => {
+      interruptAssistantVoiceQuery();
+    });
+
+    // Handle mic start request usually for immediate response
+    gassist.assistant.onRequestStartMic(() => {
+      const onPlayerWaiting = () => {
+        audioPlayer.off('player:waiting', onPlayerWaiting);
+        invokeAssistantVoiceQuery();
+      };
+
+      // If the audio player is done playing previous audio response,
+      // activate the mic
+      audioPlayer.on('player:waiting', onPlayerWaiting);
     });
 
     return () => {
-      microphone.off('mic:started');
-      microphone.off('mic:stopped');
+      microphone.off('mic:started', onMicStarted);
+      microphone.off('mic:stopped', onMicStopped);
     };
   }, []);
 
@@ -34,30 +55,43 @@ function AssistantMic() {
       type="button"
       onClick={() => {
         if (isMicActive) {
-          audioPlayer.playPingStop();
-          microphone.stop();
+          interruptAssistantVoiceQuery();
         }
         else {
-          audioPlayer.playPingStart();
-          microphone.start();
+          invokeAssistantVoiceQuery();
         }
       }}
     >
       <AssistantMicMain
         isMicActive={isMicActive}
-        showListeningBar={() => {
-          // Hide mic dots
-          setMicDotsVisibility(true);
-        }}
-        hideListeningBar={() => {
-          // Show mic dots
-          setMicDotsVisibility(false);
-        }}
+        showListeningBar={() => setMicDotsVisibility(true)}
+        hideListeningBar={() => setMicDotsVisibility(false)}
       />
 
       { (isMicDotsVisible) && <AssistantMicDots /> }
     </button>
   );
+}
+
+/**
+ * Invoke voice query & start the mic
+ */
+function invokeAssistantVoiceQuery() {
+  // Stop voice player if already playing audio response back
+  audioPlayer.stop();
+  audioPlayer.playPingStart();
+
+  // Invoke assistant for voice query and start microphone
+  gassist.assistant.invokeAssistant();
+  microphone.start();
+}
+
+/**
+ * Interrupt ongoing voice query & stop the mic
+ */
+function interruptAssistantVoiceQuery() {
+  microphone.stop();
+  audioPlayer.playPingStop();
 }
 
 export default AssistantMic;
