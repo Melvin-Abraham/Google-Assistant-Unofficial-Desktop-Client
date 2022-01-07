db31b7940916940fb38132c56966053993de8460
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-01-07 20:12:41 +0530

    feat: Handle text-based query for assistant

--- src/renderer/components/QueryBar/QueryBar.tsx
+++ src/renderer/components/QueryBar/QueryBar.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import gassist from 'gassist';
 import AssistantMic from './AssistantMic/AssistantMic';
 import './QueryBar.scss';
 
 function QueryBar() {
+  const [textQuery, setTextQuery] = useState('');
+
   return (
     <div id="query-bar-root">
       <input
@@ -10,6 +13,20 @@ function QueryBar() {
         type="text"
         id="query-bar"
         placeholder="Ask me anything..."
+        value={textQuery}
+        onChange={(event) => {
+          const query = event.target.value;
+          setTextQuery(query);
+        }}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            // Send the query to assistant
+            gassist.assistant.invokeAssistant(textQuery);
+
+            // Reset the query bar text
+            setTextQuery('');
+          }
+        }}
       />
 
       <AssistantMic />
