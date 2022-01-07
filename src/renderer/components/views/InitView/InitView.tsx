df020a3a6df5006b33715884379d26c0f31e81dd
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-01-07 20:18:27 +0530

    feat: Change initView message based on mic status

--- src/renderer/components/views/InitView/InitView.tsx
+++ src/renderer/components/views/InitView/InitView.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { microphone } from 'lib/audio/microphone';
 import AssistantLogoClear from 'res/images/assistant_logo_clear.svg?component';
 import './InitView.scss';
 
 function InitView() {
+  const [isMicActive, setIsMicActive] = useState(false);
+
+  useEffect(() => {
+    const onMicStarted = () => setIsMicActive(true);
+    const onMicStopped = () => setIsMicActive(false);
+
+    microphone.on('mic:started', onMicStarted);
+    microphone.on('mic:stopped', onMicStopped);
+
+    return () => {
+      microphone.off('mic:started', onMicStarted);
+      microphone.off('mic:stopped', onMicStopped);
+    };
+  }, []);
+
   return (
     <div className="initview-root">
       <AssistantLogoClear className="logo" />
 
       <h1 className="initview-message">
-        Hi! How can I help?
+        { (!isMicActive) ? 'Hi! How can I help?' : 'Listening...' }
       </h1>
     </div>
   );
