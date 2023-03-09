d6195db97750786191b86a317632ff0235655b16
Melvin Liji Abraham <melvinredvirus@gmail.com>
2023-03-09 21:00:40 +0530

    feat: Display assistant response in viewlet
    Display the current assistant response in response viewlet

--- src/renderer/components/ResponseViewlet/ResponseViewlet.tsx
+++ src/renderer/components/ResponseViewlet/ResponseViewlet.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import InitView from 'components/views/InitView/InitView';
+import useHistory from 'renderer/hooks/useHistory';
 import './ResponseViewlet.scss';
 
 function ResponseViewlet() {
+  const responseViewletWrapperRef = useRef<HTMLDivElement>(null);
+  const { currentHistoryItem, historyHead } = useHistory();
+
+  useEffect(() => {
+    if (
+      responseViewletWrapperRef.current === null
+      || !(currentHistoryItem?.transformedScreenData)
+    ) {
+      return;
+    }
+
+    responseViewletWrapperRef.current.innerHTML = '';
+    responseViewletWrapperRef.current.appendChild(currentHistoryItem.transformedScreenData);
+  }, [currentHistoryItem]);
+
   return (
-    <div className="viewlet-root">
-      <InitView />
+    <div className="viewlet-root" key={historyHead}>
+      {historyHead === -1 && <InitView />}
+      {currentHistoryItem !== undefined && (
+        <div
+          className="response-wrapper"
+          ref={responseViewletWrapperRef}
+        />
+      )}
     </div>
   );
 }
